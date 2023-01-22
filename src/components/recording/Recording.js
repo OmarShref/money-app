334ac6680a3d9e3be508b4f2ade8794f235699bb
Omar Sharif <mohandesomar@gmail.com>
2023-01-22 10:55:27 +0200

    third commit

--- src/components/recording/Recording.js
+++ src/components/recording/Recording.js
@@ -1,27 +1,42 @@
 import "./Recording.css";
 
 const Recording = () => {
+  var timeoutID;
+  const pushPaperPlaneAnimation = () => {
+    document.body.style.setProperty("--paper-plane-animation", "");
+    document.body.style.setProperty(
+      "--paper-plane-animation",
+      "paper-plane-animation"
+    );
+    try {
+      clearTimeout(timeoutID);
+      timeoutID = setTimeout(() => {
+        document.body.style.setProperty("--paper-plane-animation", "");
+      }, 3200);
+    } catch (error) {
+      console.log("error : ", error);
+    }
+  };
   return (
     <div className="recordings">
-      <div className="paper-plane-animation">
+      <div id="paper-plane-animation" className="paper-plane-animation">
         <i className="fa-regular fa-paper-plane"></i>
       </div>
       <div className="outputs"></div>
       <div className="inputs">
         <div className="row">
           <div className="icons">
-            <i class="fa-solid fa-calendar-days"></i>
+            <i className="fa-solid fa-calendar-days"></i>
           </div>
-          <input type="date" />
+          <input type="date" className="date" />
         </div>
         <div className="row">
           <div className="icons">
-            <i class="fa-solid fa-sack-xmark"></i>
+            <i className="fa-solid fa-sack-xmark"></i>
           </div>
-          <input type="number" placeholder="Input Your Expenses" />
+          <input type="number" placeholder="Your Expenses" />
           <select className="expenses-type">
-            <option value="Food">Food</option>
-            <option value="Food">Food</option>
+            <option value="Food">Food and Drink and whatever</option>
             <option value="Food">Food</option>
             <option value="Food">Food</option>
             <option value="Food">Food</option>
@@ -30,14 +45,17 @@ const Recording = () => {
             <option value="Food">Food</option>
           </select>
           <button>
-            <i className="fa-regular fa-paper-plane"></i>
+            <i
+              className="fa-regular fa-paper-plane"
+              onClick={pushPaperPlaneAnimation}
+            ></i>
           </button>
         </div>
         <div className="row">
           <div className="icons">
-            <i class="fa-solid fa-money-bill-trend-up"></i>
+            <i className="fa-solid fa-money-bill-trend-up"></i>
           </div>
-          <input type="number" placeholder="Input Your Income" />
+          <input type="number" placeholder="Your Income" />
           <select className="income-type">
             <option value="Food">Food</option>
             <option value="Food">Food</option>
@@ -49,16 +67,22 @@ const Recording = () => {
             <option value="Food">Food</option>
           </select>
           <button>
-            <i className="fa-regular fa-paper-plane"></i>
+            <i
+              className="fa-regular fa-paper-plane"
+              onClick={pushPaperPlaneAnimation}
+            ></i>
           </button>
         </div>
         <div className="row">
           <div className="icons">
-            <i class="fa-solid fa-book"></i>
+            <i className="fa-solid fa-book"></i>
           </div>
-          <input type="text" placeholder="Input Your Diary" />
+          <textarea rows={1} placeholder="Write Your Diary"></textarea>
           <button>
-            <i className="fa-regular fa-paper-plane"></i>
+            <i
+              className="fa-regular fa-paper-plane"
+              onClick={pushPaperPlaneAnimation}
+            ></i>
           </button>
         </div>
       </div>
